c71172c1f8f6b91d2e733bee97796975eb1e1840
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-05-10 23:21:54 +0300

    Provided qUERY CLIENT

--- RNTask/src/App.tsx
+++ RNTask/src/App.tsx
@@ -1,5 +1,6 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {Home, RestaurantDetails} from './screens';
@@ -7,22 +8,26 @@ import {lightTheme} from './theme/theme';
 
 const Stack = createNativeStackNavigator();
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <PaperProvider theme={lightTheme}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen
-            name="RestaurantDetails"
-            component={RestaurantDetails}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
+    <QueryClientProvider client={queryClient}>
+      <PaperProvider theme={lightTheme}>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen
+              name="RestaurantDetails"
+              component={RestaurantDetails}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </QueryClientProvider>
   );
 };
 
