44027d688274c82c6853d969a85947b226da95be
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-05-10 23:43:15 +0300

    swr: got data

--- RNTask/src/screens/Home/Home.tsx
+++ RNTask/src/screens/Home/Home.tsx
@@ -5,6 +5,31 @@ import {SafeAreaView, ScrollView, StatusBar, View, Text} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {TouchFiller} from '@root/components';
 import {useQuery, useQueryClient} from '@tanstack/react-query';
+import axios from 'axios';
+import useSWR from 'swr';
+
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+const token =
+  'Jw0oIMgpId1HV8x-mogAapr36SVRDSAM00qOEvAmLyxCaOV1I0T6kzJbSvazjA6Q7sNS46uHfHzRzLLAESkHYv3ES50h-sUQwtwvh836OsN-D5UwO6ObMswyxDM6YXYx';
+
+const config = {
+  headers: {Authorization: `Bearer ${token}`},
+};
+
+const bodyParameters = {
+  key: 'value',
+};
+
+const instance = axios.create({
+  baseURL: 'https://api.yelp.com/v3/',
+  timeout: 10000,
+  headers: {Authorization: 'Bearer ' + token},
+});
+
+const getData = axios
+  .get('https://api.github.com/repos/tannerlinsley/react-query')
+  .then(res => res.data);
 
 const Home = () => {
   const colors = useAppTheme().colors;
@@ -12,7 +37,23 @@ const Home = () => {
 
   const queryClient = useQueryClient();
 
-  const query = useQuery({queryKey: ['todos'], queryFn: getTodos});
+  const {data, error, isLoading} = useSWR(
+    'https://api.github.com/repos/tannerlinsley/react-query',
+    fetcher,
+  );
+
+  /*
+  const {data, isError, isFetched, error} = useQuery({
+    queryKey: ['todos'],
+    queryFn: getData,
+  });*/
+
+  console.log('data', data);
+  //console.log('isError', isError);
+  //console.log('isFetched', isFetched);
+  console.log('isLoading', isLoading);
+  console.log('error', error);
+  console.log('------------------------------------');
 
   return (
     <SafeAreaView style={{backgroundColor: colors.appBg}}>
