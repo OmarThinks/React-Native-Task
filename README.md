f4b17e802c117ae37c430cea184f3b33d8b8bf8a
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-05-10 21:25:50 +0300

    Installed RNP

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 
 
-- TODO: Make React navigation work
-- TODO: Add 2 screens
 - TODO: Add React Native papaer
 - Make rnp theme work
 - Make icons work
--- RNTask/App.tsx
+++ RNTask/App.tsx
@@ -1,13 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {SafeAreaView, ScrollView, StatusBar, Text} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
@@ -24,14 +19,16 @@ function TestScreen(): JSX.Element {
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <Stack.Screen name="Home" component={TestScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="Home" component={TestScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
