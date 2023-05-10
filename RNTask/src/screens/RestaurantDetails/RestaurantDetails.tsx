7d963d95355eb77e51505c18b62f8568ac701718
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-05-10 23:18:09 +0300

    Added Google Maps

--- RNTask/src/screens/RestaurantDetails/RestaurantDetails.tsx
+++ RNTask/src/screens/RestaurantDetails/RestaurantDetails.tsx
@@ -2,6 +2,7 @@ import {AppBar} from '@root/components';
 import React from 'react';
 import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
 import {useAppTheme} from '../../theme/theme';
+import MapView from 'react-native-maps';
 
 const RestaurantDetails = () => {
   const colors = useAppTheme().colors;
@@ -15,6 +16,19 @@ const RestaurantDetails = () => {
           minHeight: '100%',
         }}>
         <AppBar header="Restaurants" hasBack />
+
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={{
+            width: '100%',
+            height: 300,
+          }}
+        />
       </ScrollView>
     </SafeAreaView>
   );
